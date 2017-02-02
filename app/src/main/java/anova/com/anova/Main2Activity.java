be3efc89d0fee3f52582215e1074b3eb420a6cc3
Ramon Duran <ramon.duran@alaya.cl>
2017-02-02 16:47:23 -0300

    agregado funcionalidad espera

---
+++ app/src/main/java/anova/com/anova/Main2Activity.java
@@ -0,0 +1,42 @@
+package anova.com.anova;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main2Activity extends AppCompatActivity {
+
+    Main2Activity mainActivity = this;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final AlertDialog.Builder alert = new AlertDialog.Builder(mainActivity);
+                alert.setTitle("Pregunta para jose Miguel");
+                alert.setMessage("¿Eres gay?");
+
+                alert.setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        //Your action here
+                    }
+                });
+
+                alert.setNegativeButton("Si", new DialogInterface.OnClickListener() {
+                     public void onClick(DialogInterface dialog, int whichButton) {
+
+                     }
+                });
+
+                alert.show();
+            }
+        });
+    }
+}
--- app/src/main/java/anova/com/anova/MainActivity.java
+++ app/src/main/java/anova/com/anova/MainActivity.java
@@ -1,13 +1,34 @@
 package anova.com.anova;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    MainActivity mainActivity = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Thread thread = new Thread(){
+            public void run(){
+                try {
+                    this.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Intent intent = new Intent(mainActivity,Main2Activity.class);
+                startActivity(intent);
+            }
+        };
+        thread.start();
+    }
+
 }
