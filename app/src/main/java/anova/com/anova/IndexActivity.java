e92ff1abe228a6ce277f6190050d78e8c7c6e969
Ramon Duran <ramon.duran@alaya.cl>
2017-02-15 21:46:25 -0300

    agregado navigator

--- app/src/main/java/anova/com/anova/IndexActivity.java
+++ app/src/main/java/anova/com/anova/IndexActivity.java
@@ -1,66 +1,151 @@
 package anova.com.anova;
 
-import android.content.DialogInterface;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-import java.util.HashMap;
+public class IndexActivity extends ActionBarActivity {
 
-import anova.com.anova.session.SessionManager;
+    private DrawerLayout drawerLayout;
+    private ListView navList;
+    private CharSequence mTitle;
 
-public class IndexActivity extends AppCompatActivity {
-
-    IndexActivity mainActivity = this;
-
-    private TextView nombre;
-    private TextView apellido;
-    private TextView email;
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
-        Button button = (Button) findViewById(R.id.button);
-        Button cerrar_sesion = (Button) findViewById(R.id.cerrar_sesion);
-        final SessionManager sessionManager = new SessionManager(this);
-        HashMap<String,String> userdata = sessionManager.getUserDetails();
-        nombre = (TextView)findViewById(R.id.nombreUsuario) ;
-        apellido = (TextView)findViewById(R.id.apellidoUsuario) ;
-        email = (TextView)findViewById(R.id.emailUsuario) ;
-        nombre.setText(userdata.get("nombre"));
-        apellido.setText(userdata.get("apellido"));
-        email.setText(userdata.get("email"));
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final AlertDialog.Builder alert = new AlertDialog.Builder(mainActivity);
-                alert.setTitle("Pregunta para jose Miguel");
-                alert.setMessage("¿Eres gay?");
 
-                alert.setPositiveButton("Si", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int whichButton) {
-                        //Your action here
-                    }
-                });
+        mTitle = getTitle(); // Get current title
 
-                alert.setNegativeButton("Si", new DialogInterface.OnClickListener() {
-                     public void onClick(DialogInterface dialog, int whichButton) {
+        this.drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        this.navList = (ListView) findViewById(R.id.left_drawer);
 
-                     }
-                });
+        // Load an array of options names
+        final String[] names = getResources().getStringArray(
+                R.array.nav_options);
 
-                alert.show();
+        // Set previous array as adapter of the list
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, names);
+        navList.setAdapter(adapter);
+        navList.setOnItemClickListener(new DrawerItemClickListener());
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout,
+                R.drawable.ic_menu, R.string.open_drawer,
+                R.string.close_drawer) {
+
+            /**
+             * Called when a drawer has settled in a completely closed state.
+             */
+            public void onDrawerClosed(View view) {
+                getSupportActionBar().setTitle(mTitle);
+                // creates call to onPrepareOptionsMenu()
+                supportInvalidateOptionsMenu();
             }
-        });
-        cerrar_sesion.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sessionManager.logoutUser();
+
+            /**
+             * Called when a drawer has settled in a completely open state.
+             */
+            public void onDrawerOpened(View drawerView) {
+                getSupportActionBar().setTitle("Selecciona opción");
+                // creates call to onPrepareOptionsMenu()
+                supportInvalidateOptionsMenu();
             }
-        });
+        };
+
+        // Set the drawer toggle as the DrawerListener
+        drawerLayout.setDrawerListener(drawerToggle);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
     }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        // Called by the system when the device configuration changes while your
+        // activity is running
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        // Handle your other action bar items...
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    /*
+     * Called whenever we call invalidateOptionsMenu()
+     */
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // If the nav drawer is open, hide action items related to the content
+        // view
+        boolean drawerOpen = drawerLayout.isDrawerOpen(navList);
+        menu.findItem(R.id.action_search).setVisible(!drawerOpen);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    private class DrawerItemClickListener implements
+            ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position,
+                                long id) {
+            selectItem(position);
+        }
+    }
+
+    /** Swaps fragments in the main content view */
+    private void selectItem(int position) {
+        // Get text from resources
+        mTitle = getResources().getStringArray(R.array.nav_options)[position];
+
+        // Create a new fragment and specify the option to show based on
+        // position
+        NavFragment fragment = new NavFragment();
+        Bundle args = new Bundle();
+        args.putString(fragment.KEY_TEXT, mTitle.toString());
+        fragment.setArguments(args);
+
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.content_frame, fragment).commit();
+
+        // Highlight the selected item, update the title, and close the drawer
+        navList.setItemChecked(position, true);
+        getSupportActionBar().setTitle(mTitle);
+        drawerLayout.closeDrawer(navList);
+    }
+
 }
---
+++ app/src/main/java/anova/com/anova/NavFragment.java
@@ -0,0 +1,28 @@
+package anova.com.anova;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class NavFragment extends Fragment {
+
+    public final static String KEY_TEXT = "key_text";
+
+    private String mText;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        mText = getArguments().getString(KEY_TEXT);
+
+        View v = inflater.inflate(R.layout.fragment_nav, container, false);
+        TextView tv = (TextView) v.findViewById(R.id.tv_fragment);
+        tv.setText(mText);
+
+        return v;
+    }
+}
